49c8c316244341e7dc1367cfa4f5d6e0bee6d3ce
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-22 16:33:04 -0500

    Made some changes to the menu file

--- Menu_Logger.py
+++ Menu_Logger.py
@@ -1,78 +1,77 @@
-import datetime
-import sys
 import os
+import sys
+import datetime
+import Add_Work_Logger
+import Previous_Work_Logger
+from time import gmtime, strftime, sleep
 
-from time import gmtime, sleep, strftime
 
-from Add_Work_Logger import add_log
-from Previous_Work_Logger import show_previous_entries
+def menu():
+    pass
+
+def start():
+    clear()
+    user(name())
+    show_time()
+    clear()
+    display_menu()
+
+def name():
+    name = input("Enter full name please:  ")
+    clear()
+    return name
+
+def user(name):
+    current_time = datetime.datetime.now()
+    if current_time < 12:
+        print("Good morning {}, and welcome to the work logger.\n".format(name))
+    elif 12 <= current_time < 18:
+        print("Good afternoon {}, and welcome to the work logger.\n".format(name))
+    else:
+        print("Good evening {}, and welcome to the work logger.\n".format(name))
+
+def show_time():
+    the_time = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+    print("Current Time:", the_time)
+    sleep(3)
+
+def display_menu():
+    ''' Menu for the Application '''
+    while True:
+        print(''' 
+        
+        Menu
+        a) Add New Entry
+        b) Lookup Previous Entries
+        c) Quit
+        
+        ''')
+        choice = input("\n\nWhat would you like to do?  ").strip()
+        if choice.lower() in "abc":
+            if choice.lower() == 'a':
+                Add_Work_Logger.add_work_log()
+                break
+            elif choice.lower() == 'b':
+                Previous_Work_Logger.previous_entries()
+                break
+            elif choice.lower() == 'c':
+                input("Thank you come again... Press enter to leave...")
+                clear()
+                sys.exit()
+                break
+        else:
+            input("That is an invalid option... Please choose from the 3 choices a, b or c... Press enter to continue...")
+            display_menu()
+            break
 
 
 def clear():
-    """Clear The Screen"""
+    ''' Exit the system '''
     try:
         os.system('cls')
     except:
         os.system('clear')
 
-def start_app():
-    clear()
-    welcome_user(ask_for_name())
-    show_time()
-    clear()
-    show_menu()
-
-def menu():
-    """Initiates the menu"""
-    clear()
-    show_menu()
-
-def ask_for_name():
-    name = input("Enter your name please: \n")
-    clear()
-    return name
-
-def show_time():
-    print("Current Time\n")
-    time = strftime("%a, %d %b %Y %H:%M:%S ", gmtime())
-    print(time)
-    sleep(3)
-
-def welcome_user(name):
-    current_time = datetime.datetime.now()
-    if current_time.hour < 12:
-        print("Good morning {}, and welcome to my work log. \n".format(name))
-    elif 12 <= current_time.hour < 18:
-        print("Good afternoon {}, and welcome to my work log. \n".format(name))
-    else:
-        print("Good evening {}, and welcome to my work llog. \n".format(name))
-    sleep(4)
-
-def show_menu():
-    while True:
-        print("Menu ")
-        print(""" 
-                [A]dd a new entry
-                [L]ookup previous entries
-                [Q]uit
-                """)
-        option = input("\n What choice would you like to make? \n").strip()
-        if option.upper() in "ALQ":
-            if option.upper() == 'A':
-                Add_Work_Logger.add_log()
-                break
-            elif option.upper() == 'L':
-                Previous_Work_Logger.show_previous_entries()
-                break
-            elif option.upper() == 'Q':
-                clear()
-                sys.exit
-                break
-        else:
-            input("\n Invalid option. Please use the menu correctly. Use A Q or L. Press enter to continue...")
-            show_menu()
-            break
-
 
 if __name__ == '__main__':
-    start_app()
+    start()
